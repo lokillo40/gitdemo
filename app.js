54bad3439f9c36fe97b12a83195a6e07ea3c28e3
will <wavernigor@gmail.com>
2023-01-30 13:20:51 -0800

    every2 sec

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const header = document.querySelector("h1");
 const letters = header.querySelectorAll(".letter");
 
-letters.forEach((letter, index) => {
-  letter.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-});
+setInterval(() => {
+  letters.forEach((letter, index) => {
+    letter.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  });
+}, 2000);
